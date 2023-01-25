637ccc435898a07f144a4d2eecf0b41c87498abf
Ryota <ryota.t.exe@gmail.com>
2023-01-25 20:05:21 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_config_google_map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_config_google_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: GoogleMapWidget(),
+    );
+  }
+}
+
+class GoogleMapWidget extends StatefulWidget {
+  const GoogleMapWidget({super.key});
+
+  @override
+  State<GoogleMapWidget> createState() => _GoogleMapWidgetState();
+}
+
+class _GoogleMapWidgetState extends State<GoogleMapWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
